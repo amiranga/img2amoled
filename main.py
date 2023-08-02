1e73aad5f80344a8d3600430cb860bfaa73696bf
amiranga <ae.amunugama@gmail.com>
2023-08-02 23:17:06 +0400

    code format + remove line breaks

--- main.py
+++ main.py
@@ -4,14 +4,14 @@ file = "C:/Users/aeamu/Pictures/img.png"
 img = Image.open(file)
 width, height = img.size
 thresh = 128
-fn = lambda x : 255 if x > thresh else 0
+fn = lambda x: 255 if x > thresh else 0
 img = img.convert("L").point(fn, mode='1')
 
 bCount = 0
 wCount = 0
-for y in range(0, height): #each pixel has coordinates
+for y in range(0, height):  # each pixel has coordinates
     for x in range(0, width):
-        a = img.getpixel((x,y))
+        a = img.getpixel((x, y))
         if a == 255:
             wCount = wCount + 1
         else:
