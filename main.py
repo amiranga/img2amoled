39d3544561509ec700e9a231b6feaaf2b52221a0
amiranga <ae.amunugama@gmail.com>
2023-08-02 23:09:59 +0400

    invert colors conditionally

--- main.py
+++ main.py
@@ -1,8 +1,22 @@
-from PIL import Image, ImageEnhance
+from PIL import Image, ImageOps
 
-file = "C:/Users/aeamu/Pictures/img1.png"
+file = "C:/Users/aeamu/Pictures/img.png"
 img = Image.open(file)
-thresh = 150
+width, height = img.size
+thresh = 128
 fn = lambda x : 255 if x > thresh else 0
-img2 = img.convert("L").point(fn, mode='1')
-img2.show()
+img = img.convert("L").point(fn, mode='1')
+
+bCount = 0
+wCount = 0
+for y in range(0, height): #each pixel has coordinates
+    for x in range(0, width):
+        a = img.getpixel((x,y))
+        if a == 255:
+            wCount = wCount + 1
+        else:
+            bCount = bCount + 1
+
+if wCount > bCount:
+    img = ImageOps.invert(img)
+img.show()
